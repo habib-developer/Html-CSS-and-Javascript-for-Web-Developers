9266ece6db1a8ee4d031e14ebd42145054a4ae93
Habib ul Rehman <mhabibulrehman@gmail.com>
2018-11-04 22:10:12 +0500

    up

--- Module 4 Coding assignment/script.js
+++ Module 4 Coding assignment/script.js
@@ -2,7 +2,6 @@
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-debugger;
 for (var i = 0; i < names.length; i++) {
 
   var firstLetter = names[i].charAt(0).toLowerCase();
